9ab6d0d137a372f5068012bf9444081da58f3e8e
rudyorre <rudyorre@gmail.com>
2022-05-20 15:49:25 -0700

    Add argparse arguments for disabling smile, tt or mask detection

--- runner.py
+++ runner.py
@@ -15,16 +15,6 @@ import torch
 tt_model = torch.hub.load('ultralytics/yolov5', 'custom', path='./best.pt')  # or yolov5n - yolov5x6, custom
 mask_model = torch.hub.load('ultralytics/yolov5', 'custom', path='./mask_yolov5.pt')
 
-# Images
-img = './1.png'  # or file, Path, PIL, OpenCV, numpy, list
-
-# Inference
-# results = model(img)
-# Results
-# results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
-# results.show()
-# exit()
-
 # Initializing Cascade Classifiers
 haar_path = 'haas_cascade_test/'
 face_cascade = cv2.CascadeClassifier(haar_path + 'haarcascade_frontalface_default.xml')
@@ -39,17 +29,26 @@ board=Arduino(port)
 board.digital[pin].mode=SERVO
 arduino = False  
 
-def main(no_arduino=False):
+def main(
+        no_arduino=False,
+        verbose=False,
+        no_smile=False,
+        no_tt=False,
+        no_mask=False
+):
     # If arduino is enabled, initialize
     #arduino = not no_arduino
     #print('no_arduino', no_arduino)
     #print('arduino', arduino)
     #if arduino:
         #print("arduino initilaized")
-        #board=Arduino(port)
-        #board.digital[pin].mode=SERVO  
+        # board=Arduino(port)
+        # board.digital[pin].mode=SERVO  
+    smile = not no_smile
+    tt = not no_tt
+    mask = not no_mask
 
-    video_capture = cv2.VideoCapture(1)
+    video_capture = cv2.VideoCapture(0)
     smile_queue, tt_queue = [], []
     while video_capture.isOpened():
     # Captures video_capture frame by frame
@@ -58,29 +57,32 @@ def main(no_arduino=False):
         # To capture image in monochrome                   
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
         
-        # detect smile  
-        canvas, is_smiling = detect_smile(gray, frame)  
-        if len(smile_queue) > 30:
-            smile_queue.pop(0)
+        # detect smile
+        if smile:
+            frame, is_smiling = detect_smile(gray, frame)  
+            if len(smile_queue) > 30:
+                smile_queue.pop(0)
         
         # detect theta tau logo
-        canvas, is_tt = detect_tt(gray, frame)
-        if len(tt_queue) > 30:
-            tt_queue.pop(0)
+        if tt:
+            frame, is_tt = detect_tt(gray, frame)
+            if len(tt_queue) > 30:
+                tt_queue.pop(0)
 
         # Detect if mask if being worn
-        canvas, is_mask = detect_mask(gray, frame)
+        if mask:
+            frame, is_mask = detect_mask(gray, frame)
         
         #smile_queue.append(is_smiling)
         tt_queue.append(is_tt)
-        if len([True for s in smile_queue if s]) > 2 or is_tt or is_mask:
+        if (smile and len([True for s in smile_queue if s]) > 2) or (tt and is_tt) or (mask and is_mask):
             # print('is smiling')
             signal()
             smile_queue, tt_queue = [], []
 
 
         # Displays the result on camera feed                    
-        cv2.imshow('Video', canvas)
+        cv2.imshow('Video', frame)
     
         # The control breaks once q key is pressed                       
         if cv2.waitKey(1) & 0xff == ord('q'):              
@@ -100,7 +102,7 @@ def rotateservo(pin, angle):
 
 
 for k in range(0,60):
-    rotateservo(pin,k) 
+   rotateservo(pin,k) 
 
 
 def signal():
@@ -136,10 +138,6 @@ def signal():
 
     sleep(10)
 
-
-
-
-
 '''
 detectMultiScale() recommended parameters
 https://stackoverflow.com/questions/20801015/recommended-values-for-opencv-detectmultiscale-parameters
@@ -161,34 +159,64 @@ def detect_smile(gray, frame):
     return frame, is_smiling
 
 def detect_tt(gray, frame):
-    
+    '''
+    Given a frame, uses the trained model to detect the tt logo
+    and draws the predicted bounding box around the object.
+    '''
     results = tt_model(frame)
-    tt_detections = results.pandas().xyxy[0].to_json(orient="records")
+    tt_detections = results.pandas().xyxy
+    is_tt = False
 
-    # print(tt_detections)
-    is_tt = tt_detections != '[]'
-    if is_tt:
-        print('found tt!')
+    for tt_detection in tt_detections:
+        if tt_detection.name.size > 0:
+            is_tt = True
+            print('found tt!')
+            xmin, ymin, xmax, ymax = get_coords(tt_detection)
+            cv2.rectangle(frame, (xmin, ymin), ((xmax), (ymax)), (3, 239, 3), 2)
+            break
 
     return frame, is_tt
 
 def detect_mask(gray, frame):
+    '''
+    Given a frame, uses the pre-trained model to detect if the mask is worn properly
+    and draws the predicted bounding box around the object.
+    '''
     results = mask_model(frame)
-    mask_detections = results.pandas().xyxy# .to_json(orient="records")
-    
+    mask_detections = results.pandas().xyxy
     is_mask = False
 
     for mask_detection in mask_detections:
         if mask_detection.name.size > 0 and mask_detection.name.iloc[0] == 'with_mask':
             is_mask = True
             print('found mask!')
+            xmin, ymin, xmax, ymax = get_coords(mask_detection)
+            cv2.rectangle(frame, (xmin, ymin), ((xmax), (ymax)), (239, 134, 119), 2)
             break
 
     return frame, is_mask
 
+def get_coords(detection_df):
+    '''
+    Helper function that takes a Pandas DataFrame generated from a prediction,
+    and returns the coordinates that describe that bounding box.
+    '''
+    xmin = int(detection_df.xmin.iloc[0])
+    xmax = int(detection_df.xmax.iloc[0])
+    ymin = int(detection_df.ymin.iloc[0])
+    ymax = int(detection_df.ymax.iloc[0])
+    return xmin, ymin, xmax, ymax
+
 def parse_opt():
+    '''
+    Parses optional arguments for runner.py
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument('--no-arduino', action='store_true', help='true when running without arduino')
+    parser.add_argument('--verbose', action='store_true', help='prints to terminal when object is detected')
+    parser.add_argument('--no-smile', action='store_true', help='disables smile detection')
+    parser.add_argument('--no-tt', action='store_true', help='disables tt detection')
+    parser.add_argument('--no-mask', action='store_true', help='disables mask detection')
     opt = parser.parse_args()
     return opt
 
